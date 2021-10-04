e67f3af79a841acf85e1f862453f7b5580877826
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-10-03 19:03:40 -0700

    added questions as array

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "String values must be enclosed within ______ when being assigned to variables",
+        answers: [
+            "commas",
+            "curly brackets",
+            "quotes",
+            "parenthesis"
+        ],
+        correctAns: "quotes"
+    },
+    {
+        numb: 2,
+        question: "Commonly used data types DO NOT include:",
+        answers: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"
+        ],
+        correctAns: "alerts"
+    },
+    {
+        numb: 3,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: [
+            "JavaScript",
+            "terminal/bash",
+            "for loops",
+            "console.log"
+        ],
+        correctAns: "console.log"
+    },
+    {
+        numb: 4,
+        question: "The condition in an if/else statement is enclosed with",
+        answers: [
+            "quotes",
+            "curly brackets",
+            "parenthesis",
+            "square brackets"
+        ],
+        correctAns: "parenthesis"
+    },
+    {
+        numb: 5,
+        question: "Arrays in JavaScript can be used to store ______",
+        answers: [
+            "numbers and arrays",
+            "other arrays",
+            "booleans",
+            "all of the above"
+        ],
+        correctAns: "all of the above"
+    },
+];
